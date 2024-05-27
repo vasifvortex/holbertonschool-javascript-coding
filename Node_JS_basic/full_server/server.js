f2777791cc7f9aaef8cb5846384590abd59d5955
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-05-27 15:29:19 +0400

    Add files via upload

---
+++ Node_JS_basic/full_server/server.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+import express from "express";
+import mapRoutes from "./routes";
+
+const app = express();
+const PORT = 1245;
+
+mapRoutes(app);
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+
+export default app;
+module.exports = app;
